ff3d7337baf98bb64cb2bcabad8a1b5e06a5727a
Kugge <sofiane.djerbi38@gmail.com>
2023-04-24 17:18:42 +0200

    Startup scripts

---
+++ server.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+GRAALPATH="export PATH=/usr/local/bin/graalvm/bin:$PATH; export JAVA_HOME=/usr/local/bin/graalvm"
+RAM_FLAGS="-Xmx24G -Xms24G"
+GRAALVM_FLAGS="-XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseNUMA -XX:AllocatePrefetchStyle=3 -XX:NmethodSweepActivity=1 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:-DontCompileHugeMethods -XX:+PerfDisableSharedMem -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:+EagerJVMCI -Dgraal.TuneInlinerExploration=1 -Dgraal.CompilerConfiguration=enterprise"
+GC_FLAGS="-XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1MaxNewSizePercent=50 -XX:G1NewSizePercent=40 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150"
+EXTRA_FLAGS="-XX:ConcGCThreads=2 --add-modules=jdk.incubator.vector -XX:+UseFastUnorderedTimeStamps"
+TIME=10
+
+
+while [ true ]; do
+    $GRAALPATH && java $RAM_FLAGS $GRAALVM_FLAGS $GC_FLAGS -jar ~/srv/server.jar nogui
+    if [[ ! -d "exit_codes" ]]; then
+        mkdir "exit_codes";
+    fi
+    if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
+        touch "exit_codes/server_exit_codes.log";
+    fi
+    echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
+    echo "----- Press enter to prevent the server from restarting in $TIME seconds -----";
+    read -t $TIME input;
+    if [ $? == 0 ]; then
+        break;
+    else
+        echo "------------------- SERVER RESTARTS -------------------";
+    fi
+done
---
+++ start.sh
@@ -0,0 +1,1 @@
+screen -dm -S minecraft /bin/bash /home/minecraft/srv/server.sh
