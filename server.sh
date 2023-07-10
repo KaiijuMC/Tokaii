615f6a3065d273fac8120cc14b7eac3e8be47c75
Kugge <sofiane.djerbi38@gmail.com>
2023-07-10 13:06:52 +0200

    Update config

--- server.sh
+++ server.sh
@@ -6,21 +6,4 @@ GC_FLAGS="-XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions
 EXTRA_FLAGS="-XX:ConcGCThreads=2 --add-modules=jdk.incubator.vector -XX:+UseFastUnorderedTimeStamps"
 TIME=10
 
-
-while [ true ]; do
-    $GRAALPATH && java $RAM_FLAGS $GRAALVM_FLAGS $GC_FLAGS -jar ~/kaiiju/server.jar nogui
-    if [[ ! -d "exit_codes" ]]; then
-        mkdir "exit_codes";
-    fi
-    if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
-        touch "exit_codes/server_exit_codes.log";
-    fi
-    echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
-    echo "----- Press enter to prevent the server from restarting in $TIME seconds -----";
-    read -t $TIME input;
-    if [ $? == 0 ]; then
-        break;
-    else
-        echo "------------------- SERVER RESTARTS -------------------";
-    fi
-done
+$GRAALPATH && java $RAM_FLAGS $GRAALVM_FLAGS $GC_FLAGS -jar ~/kaiiju/server.jar nogui
